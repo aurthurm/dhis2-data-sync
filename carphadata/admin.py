780ce8e2ea247c4ab89e537d255ae6970f534b7e
Aurthur Musendame <aurthurmusendame@gmail.com>
2023-12-21 15:10:22 +0200

    renamed the admin portal

---
+++ carphadata/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+admin.site.site_header = "Carpha Data Upload Admin"
+admin.site.site_title = "Carpha Upload Administration"
+admin.site.index_title = "Welcome to Carpha Upload Administration"
--- carphadata/urls.py
+++ carphadata/urls.py
@@ -14,13 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+# from django.contrib import admin
 from django.urls import path , include
+from .admin import admin
 
 app_name = "carphadata"
 
 urlpatterns = [
     path("", include("scrapper.urls")),
-    path("admin/", admin.site.urls),
+    path("admin/", admin.site.urls), 
     path("accounts/", include("django.contrib.auth.urls")),
 ]
